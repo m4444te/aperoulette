64985cbb6268313cebd023f811c1e8ab7b088731
Mathieu Mescam <mate@e.email>
2024-07-16 18:39:59 +0200

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+# Liste des lieux possibles
+lieux = [
+    "Au coin d'la rue",
+    "Kaf Kerin",
+    "La Pam",
+    "Le Beaj Kafé"
+]
+
+# Choisir un lieu au hasard
+lieu_choisi = random.choice(lieux)
+
+print("Le lieu choisi pour l'apéro est :", lieu_choisi)
+
